eaddd2dbac21b9f86a412bc6ddc2e37ef4e42b7d
Max Desyatov <max.desyatov@gmail.com>
2010-10-27 19:06:26 +0300

    Added low-level document structure tree displaying

--- src/main/scala/pdf/Debugger.scala
+++ src/main/scala/pdf/Debugger.scala
@@ -1,50 +1,115 @@
 package pdf
 
-import org.apache.pdfbox.exceptions.InvalidPasswordException
+import org.apache.pdfbox.pdmodel._
 
-import org.apache.pdfbox.pdfviewer.PDFTreeModel
-import org.apache.pdfbox.pdfviewer.PDFTreeCellRenderer
-import org.apache.pdfbox.pdfviewer.ArrayEntry
-import org.apache.pdfbox.pdfviewer.MapEntry
+import org.apache.pdfbox.cos._
 
-import org.apache.pdfbox.pdmodel.PDDocument
-
-import org.apache.pdfbox.util.ExtensionFileFilter
-
-import org.apache.pdfbox.cos.COSBoolean
-import org.apache.pdfbox.cos.COSFloat
-import org.apache.pdfbox.cos.COSInteger
-import org.apache.pdfbox.cos.COSName
-import org.apache.pdfbox.cos.COSNull
-import org.apache.pdfbox.cos.COSStream
-import org.apache.pdfbox.cos.COSString
-
-import javax.swing.tree.TreeModel
-import javax.swing.tree.TreePath
-import javax.swing.JFileChooser
-import javax.swing.JScrollPane
-import javax.swing.JPanel
-import javax.swing.UIManager
-
-import java.io.ByteArrayOutputStream
 import java.io.File
-import java.io.FileInputStream
-import java.io.InputStream
-import java.io.IOException
 
 import swing._
-import swing.event._
+
+import javax.swing.event.{HyperlinkListener, HyperlinkEvent}
+
+import au.ken.treeview._
+import au.ken.treeview.Tree._
+import au.ken.treeview.event._
+
+import scalaj.collection.Imports._
+
+case class PDFTreeNode(var obj: COSBase) {
+  def typeString = obj match {
+    case d: COSDocument   => "Document"
+    case s: COSString     => "String"
+    case a: COSArray      => "Array"
+    case s: COSStream     => "Stream"
+    case d: COSDictionary => "Dictionary"
+    case b: COSBoolean    => "Boolean"
+    case n: COSName       => "Name"
+    case n: COSNumber     => "Number"
+    case o                => o.getClass.getName
+  }
+
+  def children: Seq[PDFTreeNode] = obj match {
+    case d: COSDocument   => d.getObjects.asScala map (o => new PDFTreeNode(o.getObject))
+//    case a: COSArray      => a.toList.asScala map (o => new PDFTreeNode(o))
+//    case d: COSDictionary => d.keySet.asScala.toList map (o => new PDFTreeNode(o))
+    case _                => List()
+  }
+
+  override def toString: String = obj match {
+    case o: COSObject     => o.toString
+    case d: COSDocument   => d.getHeaderString + "\n" +
+            new PDFTreeNode(d.getCatalog.getObject) + "\n" +
+            new PDFTreeNode(d.getTrailer)
+    case s: COSString     => "(" + s.getString + ")"
+    case a: COSArray      => "[" + a.toList.asScala.map(o => new PDFTreeNode(o).toString).mkString(" ") + "]"
+    case s: COSStream     => s.getStreamTokens.asScala mkString
+    case d: COSDictionary => "<<" + d.entrySet.asScala.map(e => new PDFTreeNode(e.getKey) + " " +
+              new PDFTreeNode(e.getValue)).mkString("\n") + ">>"
+    case b: COSBoolean    => b.getValue.toString
+    case n: COSName       => "/" + n.getName
+    case i: COSInteger    => i.intValue.toString
+    case n: COSNumber     => n.doubleValue.toString
+    case o                => o.toString
+  }
+}
 
 object Debugger extends SimpleSwingApplication {
-  val click = new Button {
-      text = "click"
-    }
+  var doc = new PDDocument
+
   def top = new MainFrame {
     title = "PDFDebugger"
-    contents = new FlowPanel(click)
-  }
-  listenTo(click)
-  reactions += {
-    case ButtonClicked(`click`) => quit()
+    menuBar = new MenuBar
+
+    val displayWidget = new EditorPane() {//"text/html", "") {
+      editable = false
+      peer.addHyperlinkListener(new HyperlinkListener() {
+        def hyperlinkUpdate(ev: HyperlinkEvent) {
+          if (ev.getEventType() == HyperlinkEvent.EventType.ACTIVATED)
+            peer.setPage(ev.getURL())
+        }
+      })
+    }
+
+    def readPDFFile(f: File): Unit = {
+      doc.close()
+      doc = PDDocument.load(f)
+      val rootNode = new PDFTreeNode(doc.getDocument)
+      tree.treeData = new TreeModel[PDFTreeNode](List(rootNode), _.children)
+      displayWidget.text = rootNode.toString
+    }
+    val fileMenu = new Menu("File")
+    fileMenu.contents ++= List(new MenuItem(Action("Open...") {
+        val chooser = new FileChooser
+        if (FileChooser.Result.Approve == chooser.showOpenDialog(contents(0)))
+          readPDFFile(chooser.selectedFile)
+      }),
+      new MenuItem(Action("Exit") {quit()})
+      )
+    menuBar.contents += fileMenu
+
+    lazy val tree = new Tree[PDFTreeNode] {
+      renderer = Renderer(_.typeString)
+
+      listenTo(selection)//, mouse.clicks) //editor, selection, mouse.clicks)
+
+      reactions += {
+        case TreePathSelected(_, _, _, newSelection, _) =>
+          displayWidget.text = {
+            val maybeLast = newSelection.getOrElse(List()).lastOption
+            maybeLast.map(n => n.asInstanceOf[PDFTreeNode].toString) getOrElse ""
+          }
+      }
+    }
+
+    tree.expandAll
+
+    val mainPanel = new BoxPanel(Orientation.Vertical)
+    val centralPanel = new SplitPane(Orientation.Vertical,
+      new ScrollPane(tree),
+      new ScrollPane(displayWidget))
+    val statusPanel = new Label("this is a statusbar")
+    mainPanel.contents ++= List(centralPanel, statusPanel)
+    contents = mainPanel
   }
 }
